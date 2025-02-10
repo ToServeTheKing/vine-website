a6ecff81d0f76c9acb2bbc77f964c4b795a15aef
Austin Bennett <austin@thebennett.net>
2025-02-10 12:42:19 -0600

    updated default.nix shell for nextjs

--- default.nix
+++ default.nix
@@ -1,15 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
-
 let
-    nodePackages = pkgs.nodePackages;
+  pkgs = import <nixpkgs> {};
 in
-pkgs.mkShell {
-    buildInputs = [
-        nodePackages.nodejs
-        nodePackages.yarn
-    ];
 
-    shellHook = ''
-        echo "Welcome to the bakery-template development environment!"
-    '';
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.nodejs
+    pkgs.yarn
+  ];
+
+  # Install Next.js via yarn in the shell
+  shellHook = ''
+    if ! [ -d "node_modules" ]; then
+      yarn add next react react-dom
+    fi
+  '';
 }
