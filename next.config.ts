c1e3390cdb2e945934cbaf9ebd9407ab808167f5
Austin <austin@therobustroast.com>
2026-03-14 11:32:25 -0500

    updated hours

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
   }
 };
 
